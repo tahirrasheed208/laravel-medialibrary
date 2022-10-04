056714dae2e0b67d3398683ead06b4975476f9e7
tahirrasheed208 <tahirrasheed208@gmail.com>
2022-10-04 21:25:59 +0500

    remove facade binding

--- src/MediaLibraryServiceProvider.php
+++ src/MediaLibraryServiceProvider.php
@@ -19,10 +19,6 @@ class MediaLibraryServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(
             __DIR__.'/../config/medialibrary.php', 'medialibrary'
         );
-
-        $this->app->singleton('media', function () {
-            return new MediaHelper();
-        });
     }
 
     /**
